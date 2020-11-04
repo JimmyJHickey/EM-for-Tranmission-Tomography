d1d0fafcab4cd224fca9bf72f4e0096b2b9db844
jimmyjhickey <jimmyjhickey@gmail.com>
2020-11-04 14:39:07 -0500

    Made into function

--- jimmy/pixel_circle.R
+++ jimmy/pixel_circle.R
@@ -1,19 +1,19 @@
-library("plot.matrix")
-
-radius = 5
-center = (2 * radius + 1 + 1) / 2
-nrow =  2 * radius + 1
-ncol =  2 * radius + 1
-
-in_circ = matrix(0, nrow = nrow, ncol = ncol)
-
-for(i in 1:nrow){
-  for(j in 1:ncol){
-    if( sqrt( (i - center)^2 + (j - center)^2 ) <= radius ){
-      in_circ[i,j] = 1
+in_circle = function(radius){
+  radius = 5
+  center = (2 * radius + 1 + 1) / 2
+  nrow =  2 * radius + 1
+  ncol =  2 * radius + 1
+  
+  in_circ = matrix(0, nrow = nrow, ncol = ncol)
+  
+  for(i in 1:nrow){
+    for(j in 1:ncol){
+      if( sqrt( (i - center)^2 + (j - center)^2 ) <= radius ){
+        in_circ[i,j] = 1
+      }
     }
   }
+  
+  return(in_circ)
 }
-
-plot(in_circ)
 
