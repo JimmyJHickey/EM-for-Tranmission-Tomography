86f1d9a7e4dcaedbd8e25d81f420c09eabdf69af
jimmyjhickey <jimmyjhickey@gmail.com>
2020-11-04 19:53:57 -0500

    added more scan patterns and comments

--- jimmy/pixel_circle.R
+++ jimmy/pixel_circle.R
@@ -1,19 +1,21 @@
 in_circle = function(radius){
-  radius = 5
   center = (2 * radius + 1 + 1) / 2
   nrow =  2 * radius + 1
   ncol =  2 * radius + 1
   
-  in_circ = matrix(0, nrow = nrow, ncol = ncol)
+  # start everything at -1
+  in_circ = matrix(-1, nrow = nrow, ncol = ncol)
   
-  for(i in 1:nrow){
-    for(j in 1:ncol){
-      if( sqrt( (i - center)^2 + (j - center)^2 ) <= radius ){
-        in_circ[i,j] = 1
+  for(x in 1:nrow){
+    for(y in 1:ncol){
+      # if within the circle radius
+      if( sqrt( (x - center)^2 + (y - center)^2 ) <= radius ){
+        in_circ[x,y] = 0
       }
     }
   }
   
+  # if in the circle, the value is 0, otherwise the value is -1
   return(in_circ)
 }
 
--- jimmy/plotting.R
+++ jimmy/plotting.R
@@ -1,13 +1,15 @@
 plot_matrix = function(in_mat){
   require(ggplot2)
 
-  x = paste0("X", seq(1, nrow(in_mat)))
-  y = paste0("Y", seq(1, ncol(in_mat)))
+  x = seq(1, nrow(in_mat))
+  y = seq(1, ncol(in_mat))
   
   df = expand.grid(X=x, Y=y)
   df$Z = c(in_mat)
 
-  
+  # doesn't seem to care about those colors at all, but it works
   ggplot(df, aes(X, Y, fill= Z)) + 
-    geom_tile()
+    geom_tile()+
+    scale_color_manual(values = c("(-Inf,0]" = "black",
+                                    "(0,Inf]" = "grey50"))
 }
