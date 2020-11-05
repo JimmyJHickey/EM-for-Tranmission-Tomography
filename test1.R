1bc0474877618b462a9147c5ec7eb6b0f1f065c9
AlvinSheng <asheng1067@gmail.com>
2020-11-04 19:45:12 -0500

    Increasing the precision and added test for 2x2 matrix

--- test1.R
+++ test1.R
@@ -1,4 +1,6 @@
 
+# test for 3x3 matrix
+
 set.seed(708)
 
 theta <- matrix(runif(9, 0, 1), nrow = 3)
@@ -25,5 +27,33 @@ theta_init <- matrix(runif(9, 0, 1), nrow = 3)
 
 
 
-em_alg(proj_list, theta_init, .001)
+em_alg(proj_list, theta_init, .00001)
+
+
+
+# test for 2x2 matrix
+
+set.seed(740)
+
+theta <- matrix(runif(4, 0, 1), nrow = 2)
+
+dat <- data_gen_df(theta, d = 100, ROW = 1:2, COL = 1:2)
+
+proj1 <- list(d = 100, idx = c(1, 3), y = 24)
+
+proj2 <- list(d = 100, idx = c(2, 4), y = 32)
+
+proj3 <- list(d = 100, idx = c(1, 2), y = 48)
+
+proj4 <- list(d = 100, idx = c(3, 4), y = 16)
+
+proj_list <- list(proj1, proj2, proj3, proj4)
+
+
+
+theta_init <- matrix(runif(4, 0, 1), nrow = 2)
+
+
+
+em_alg(proj_list, theta_init, .00001)
 
