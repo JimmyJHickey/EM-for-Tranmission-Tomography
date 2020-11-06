42cc2539151fada77c452b017a01e2cf6b2abacd
eyanchenko <37027295+eyanchenko@users.noreply.github.com>
2020-11-06 13:23:38 -0500

    Add files via upload

--- em_mc_sim.R
+++ em_mc_sim.R
@@ -1,81 +1,119 @@
 
-# DO EVERYTHING 10 TIMES 
+#PLEASE READ 
+#This is our code to run our MC Experiments
+#Each of us will run this code for a DIFFERENT circle_theta
+#Checkered == Eric; One Cirlce == Alvin; Two Circles == Jimmy
+#Thus, when we are all done, we will have one array each, three arrays total, one per circle_theta
 
-### fifth test, 5x the repetitions, same parameters as last test otherwise. 
+##--------------------------------------------------------------
+##--------------------------------------------------------------
 
-# read in all the functions
-
+#Read in Jimmy's Files
 source("jimmy/pixel_circle.R")
 source("jimmy/plotting.R")
 source("jimmy/scan_patterns.R")
-
 source("Simulate Data.R")
-
 source("em_alg.R")
 
-# generating the theta matrix
+##--------------------------------------------------------------
+##--------------------------------------------------------------
 
-set.seed(909) # set the seed so that exactly same matrix is generated as the last test
+set.seed(27605) # set the seed 
 
-# VARY THIS
-radius = 10
+#Set the names for the array
+names.radius = c("rad3", "rad5", "rad10")
+names.reps = c("reps1", "reps2", "reps3", "reps4", "reps5")
+names.met = c("MSE", "Spectral", "Iterations")
+names.N = c("N1", "N2", "N3", "N4", "N5", "N6", "N7", "N8", "N9", "N10")
 
-in_circ = in_circle(radius)
-plot_matrix(in_circ)
+names.list = list(names.radius, names.reps, names.met, names.N)
 
-# CHANGE TO DIFFERENT THETA PATTERNS
-circle_theta  = circle_pattern(in_circ, 3, 10, 10)
-plot_matrix(circle_theta)
+#Array to hold all of our results
+#Rename to match your circle_theta name
+checker_results <- array(NaN, dim =  c(3,5,3,10), dimnames = names.list)
 
-# generating observations
+#Output is a mulit-dimensional array
+#Dim 1: Radius. Length=3 for radius=c(3,5,10)
+#Dim 2: Reps. Length=5 for reps=1:5
+#Dim 3: Metrics. Length=3 for metric = c('MSE', 'Spectral', 'Iterations')
+#Dim 4: Monte Carlo N. length=N=10
+#Thus, we willl have an 3x5x3x10 array (=450 data points)
 
-set.seed(749)
+#Set the radius and rep numbers to loop over
+radius.seq = c(3,5,10)
+reps.seq = 1:5
 
-# vector of boundary rows/columns that aren't solely negative space
-bounds <- 2:(nrow(circle_theta) - 1)
+##--------------------------------------------------------------
+##--------------------------------------------------------------
+for(radius in radius.seq){
+  #Counter to know where to put the answers
+  a = 1
+  #Generating the circle_theta matrix. Different for each of us
+  in_circ = in_circle(radius)
+  circle_theta  = checker_pattern(in_circ)
+  
+  for(reps in reps.seq){
+    b = 1
+    for(N in 1:10){
+      # generating observations
+      set.seed(as.numeric(ceiling(proc.time()[3])))
+      # vector of boundary rows/columns that aren't solely negative space
+      bounds <- 2:(nrow(circle_theta) - 1)
+      
+      #Generate data
+      proj_list <- data_gen_df(circle_theta, d = 1000000000, ROW = c(bounds, -bounds), 
+                               COL = c(bounds, -bounds),
+                               reps = reps)
+      
+      #Check for no 0 values. If so, regenerate data
+      while(y_zero(proj_list)){
+        proj_list <- data_gen_df(circle_theta, d = 1000000000, ROW = c(bounds, -bounds), 
+                                 COL = c(bounds, -bounds),
+                                 reps = reps)        
+      }
+      
+      # how many nonnegative numbers are in circle_theta?
+      num_pixel <- sum(circle_theta >= 0)
+      
+      # copy the true circle_theta's negative space, but change the nonnegative
+      # values randomly
+      circle_theta_init <- circle_theta
+      
+      circle_theta_init[which(circle_theta >= 0)] <- runif(num_pixel, 0, 0.1)
+      
+      #Run Algorithm
+      em_r10_circle_reps5 <- em_alg(proj_list, circle_theta_init, .0001) 
+      
 
-# VARY THIS
-proj_list <- data_gen_df(circle_theta, d = 1000000000, ROW = c(bounds, -bounds), 
-                         COL = c(bounds, -bounds),
-                         reps = 5)
-
-# running EM algorithm
-
-# there are no projections with zero observations
-y_zero(proj_list)
-
-# how many nonnegative numbers are in circle_theta?
-(num_pixel <- sum(circle_theta >= 0))
-
-# copy the true theta's negative space, but change the nonnegative
-# values randomly
-theta_init <- circle_theta
-
-theta_init[which(circle_theta >= 0)] <- runif(num_pixel, 0, 0.1)
-
-system.time(em_r10_circle_reps5 <- em_alg(proj_list, theta_init, .0001)) # 92.35897 minutes
-
-em_r10_circle_reps5$ctr # 324 iterations
-
-save(em_r10_circle_reps5, file = "em_results/em_r10_circle_reps5.RData")
-
-plot_matrix(em_r10_circle_reps5$theta_est)
-
-abs_diff_mat <- abs(em_r10_circle_reps5$theta_est - circle_theta)
-
-sq_diff_mat <- abs_diff_mat^2
-
-plot_matrix(abs_diff_mat)
-
-plot_matrix(sq_diff_mat)
-
-# MSE:
-
-sqrt(sum(sq_diff_mat)) / num_pixel # 0.167909
-# 0.09065642
+      
+      abs_diff_mat <- abs(em_r10_circle_reps5$theta_est - circle_theta)
+    
+      sq_diff_mat <- abs_diff_mat^2
+      
+      #plot_matrix(abs_diff_mat)
+      
+      #plot_matrix(sq_diff_mat)
+      
+      # MSE:
+      checker_results[a,b,1,N] <- sqrt(sum(sq_diff_mat)) / num_pixel 
+      #Spectral Norm
+      checker_results[a,b,2,N] <-svd(em_r10_circle_reps5$theta_est - circle_theta)$d[1]
+      #Number of iterations
+      checker_results[a,b,3,N] <-em_r10_circle_reps5$ctr 
+    }
+    b = b+1  
+  }
+  a = a+1
+}
+  
 
 
-svd(em_r10_circle_reps5$theta_est - circle_theta)$d[1]
-# 6.958846e+00
+
+
+
+
+
+
+
 
 
