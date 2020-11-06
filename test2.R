10a5a614fc1c2f91533c7b833a43788166e46219
AlvinSheng <asheng1067@gmail.com>
2020-11-05 22:41:09 -0500

    adjusting 4th test before running overnight

--- test2.R
+++ test2.R
@@ -1,5 +1,5 @@
 
-# read in all the code
+# read in all the functions
 
 source("jimmy/pixel_circle.R")
 source("jimmy/plotting.R")
@@ -109,7 +109,7 @@ theta_init <- circle_theta
 
 theta_init[which(circle_theta >= 0)] <- runif(num_pixel, 0, 0.1)
 
-em_r5_circle_reps2 <- em_alg(proj_list, theta_init, .001) # less than 2 minutes
+system.time(em_r5_circle_reps2 <- em_alg(proj_list, theta_init, .001)) # took 1.03 minutes
 
 save(em_r5_circle_reps2, file = "em_results/em_r5_circle_reps2.RData")
 
@@ -134,6 +134,16 @@ mean(sq_diff_mat) # 0.1727511
 
 
 ### third test, double the radius, same parameters as first test otherwise
+
+# read in all the functions
+
+source("jimmy/pixel_circle.R")
+source("jimmy/plotting.R")
+source("jimmy/scan_patterns.R")
+
+source("Simulate Data.R")
+
+source("em_alg.R")
 
 # generating the theta matrix
 
@@ -172,7 +182,7 @@ theta_init <- circle_theta
 
 theta_init[which(circle_theta >= 0)] <- runif(num_pixel, 0, 0.1)
 
-em_r10_circle <- em_alg(proj_list, theta_init, .001) # less than 2 minutes
+system.time(em_r10_circle <- em_alg(proj_list, theta_init, .0001)) # 16.39692 minutes
 
 save(em_r10_circle, file = "em_results/em_r10_circle.RData")
 
@@ -188,9 +198,79 @@ plot_matrix(sq_diff_mat)
 
 # MSE:
 
-mean(sq_diff_mat) # 0.1727511
+mean(sq_diff_mat) # 0.1326812
 
-# adding one more repetition does decrease MSE
+
+
+
+
+### fourth test, 10x the repetitions, same parameters as last test otherwise
+
+# read in all the functions
+
+source("jimmy/pixel_circle.R")
+source("jimmy/plotting.R")
+source("jimmy/scan_patterns.R")
+
+source("Simulate Data.R")
+
+source("em_alg.R")
+
+# generating the theta matrix
+
+set.seed(909) # set the seed so that exactly same matrix is generated as the last test
+
+radius = 10
+
+in_circ = in_circle(radius)
+plot_matrix(in_circ)
+
+circle_theta  = circle_pattern(in_circ, 3, 10, 10)
+plot_matrix(circle_theta)
+
+# generating observations
+
+set.seed(937)
+
+# vector of boundary rows/columns that aren't solely negative space
+bounds <- 2:(nrow(circle_theta) - 1)
+
+proj_list <- data_gen_df(circle_theta, d = 1000000000, ROW = c(bounds, -bounds), 
+                         COL = c(bounds, -bounds),
+                         reps = 10)
+
+# running EM algorithm
+
+# there are no projections with zero observations
+y_zero(proj_list)
+
+# how many nonnegative numbers are in circle_theta?
+(num_pixel <- sum(circle_theta >= 0))
+
+# copy the true theta's negative space, but change the nonnegative
+# values randomly
+theta_init <- circle_theta
+
+theta_init[which(circle_theta >= 0)] <- runif(num_pixel, 0, 0.1)
+
+system.time(em_r10_circle_reps10 <- em_alg(proj_list, theta_init, .0001)) 
+
+save(em_r10_circle_reps10, file = "em_results/em_r10_circle_reps10.RData")
+
+plot_matrix(em_r10_circle_reps10$theta_est)
+
+abs_diff_mat <- abs(em_r10_circle_reps10$theta_est - circle_theta)
+
+sq_diff_mat <- abs_diff_mat^2
+
+plot_matrix(abs_diff_mat)
+
+plot_matrix(sq_diff_mat)
+
+# MSE:
+
+mean(sq_diff_mat) 
+
 
 
 
